8a79e492c8b5fbf404cb945c08aa8b202224d470
harsh <hanzo4679@gmail.com>
2025-06-28 13:20:06 +0530

    refactor: improved code quality

---
+++ backend/src/index.ts
@@ -0,0 +1,34 @@
+import express, { Application, Request, Response } from 'express';
+import "dotenv/config.js";
+import { DBConnection } from './db.js';
+import { Authenticate } from './middleware/authentication.js';
+
+
+try {
+  DBConnection();
+} catch (error) {
+  console.error("Database connection failed:", error);
+  process.exit(1);
+}
+
+const app: Application = express();
+app.use("/", Authenticate);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+import SubmissionRoutes from './routes/submissionRoutes.js';
+import ProblemsRoutes from './routes/problemRoutes.js';
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "working" });
+  return;
+})
+app.use('/api/submissions', SubmissionRoutes);
+app.use('/api/problems', ProblemsRoutes);
+
+
+
+app.listen(PORT, () => {
+  console.log('server is RUNNING ON', PORT);
+})
