40cff19c6ff30de2a867dd89aee51091ad71e14c
harsh <hanzo4679@gmail.com>
2025-07-04 23:39:56 +0530

    fix: fixrunTests logic

--- compiler/src/controllers/runTests.ts
+++ compiler/src/controllers/runTests.ts
@@ -8,23 +8,30 @@ export function runTests(output: string,
     verdict: string,
     testsPassed: number,
   } {
-  console.log("testing:" , output, " ", expectedOutput);
-  if (output.length != expectedOutput.length)
+  if (output.length != expectedOutput.length) {
     return {
       verdict: "Runtime Error",
       testsPassed: -1,
     }
+  }
 
   const n: number = output.length;
   let lines: number = 0;
-  let fail: Boolean = false;
+  let fail: boolean = false;
   let testsPassed: number = 0;
 
   for (let i = 0; i < n; i++) {
-    if (expectedOutput[i] == '\n') lines += 1;
-    if (lines % linesPerTest) if (!fail) testsPassed++;
     if (expectedOutput[i] != output[i]) fail = true;
+    if (output[i] === '\n') {
+      lines++;
+      if (lines % linesPerTest == 0) {
+        if (fail) break;
+        testsPassed++;
+      }
+    }
+    // console.log("\n\nno, of lines seen: ", lines, "\ndid it fail?: ", fail, "\nLines per test: ", linesPerTest, "\nnumber of tests passed: ", testsPassed);
   }
+
   return {
     verdict: (fail ? "Wrong Answer" : "Accepted"),
     testsPassed: testsPassed,
